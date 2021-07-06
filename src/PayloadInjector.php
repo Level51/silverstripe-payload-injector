16f202f56ef05b7bb187161663c7906aeba1ec07
Daniel Kliemsch <dk@lvl51.de>
2021-07-06 17:59:39 +0200

    Add unstage function

--- src/PayloadInjector.php
+++ src/PayloadInjector.php
@@ -43,6 +43,35 @@ class PayloadInjector {
     }
 
     /**
+     * Remove previously staged keys if found.
+     *
+     * @param string|string[] $keys The key(s) to remove, in dot syntax if nested
+     */
+    public function unstage($keys) {
+       if (is_array($this->payload) && !empty($this->payload)) {
+           if (is_string($keys)) {
+               $keys = [$keys];
+           }
+
+           foreach ($keys as $key) {
+               $keyParts = explode('.', $key);
+               $lastKey = array_pop($keyParts);
+
+               $tmp =& $this->payload;
+               foreach ($keyParts as $part) {
+                   if (!isset($tmp[$part])) {
+                       break;
+                   }
+
+                   $tmp =& $tmp[$part];
+               }
+
+               unset($tmp[$lastKey]);
+           }
+        }
+    }
+
+    /**
      * Clear previously staged data
      */
     public function clearStage() {
